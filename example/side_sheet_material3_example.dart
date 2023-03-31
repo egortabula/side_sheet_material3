1b7449f0372617d1a70129433bb3dab8676e0754
Egor Tabula <64892468+egortabula@users.noreply.github.com>
2023-03-31 15:39:57 +0700

    init example

--- example/side_sheet_material3_example.dart
+++ example/side_sheet_material3_example.dart
@@ -1,5 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:side_sheet_material3/side_sheet_material3.dart';
 
-// void main() {
-//   var awesome = Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: ThemeData(
+        useMaterial3: true, // Important
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton(
+            onPressed: () {
+              showModalSideSheet(
+                // Required
+                context,
+                // Pass your content widget (required)
+                body: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                ),
+
+                // Required
+                header: 'Edit user',
+
+                barrierDismissible: false,
+                addBackIconButton: false,
+                addCloseIconButton: true,
+                addActions: true,
+                addDivider: true,
+                confirmActionTitle: 'Update',
+                cancelActionTitle: 'Cancel',
+                confirmActionOnPressed: () {},
+                transitionDuration: Duration(milliseconds: 500),
+                closeButtonTooltip: 'Close',
+                backButtonTooltip: 'Back',
+              );
+            },
+            child: Text('show side sheet'),
+          ),
+        ),
+      ),
+    );
+  }
+}
