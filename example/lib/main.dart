d1e3492d210df4086e9b41f354c3b711e302af2f
egortabula <egor.vnl@gmail.com>
2023-10-06 00:13:16 +0300

    init

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,45 +13,61 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true, // Important
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: FilledButton(
-            onPressed: () {
-              showModalSideSheet(
-                // Required
-                context,
-                // Pass your content widget (required)
-                body: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                ),
+      home: const HomeWidget(),
+    );
+  }
+}
 
-                // Required
-                header: 'Edit user',
-                barrierDismissible: false,
-                addBackIconButton: false,
-                addCloseIconButton: true,
-                addActions: true,
-                addDivider: true,
-                confirmActionTitle: 'Update',
-                cancelActionTitle: 'Cancel',
-                confirmActionOnPressed: () {},
-                transitionDuration: const Duration(milliseconds: 500),
-                closeButtonTooltip: 'Close',
-                backButtonTooltip: 'Back',
-                onDismiss: () {
-                  debugPrint('on dismiss event');
-                },
-                onClose: () {
-                  Navigator.pop(context);
-                  debugPrint('on close event');
-                },
-              );
-            },
-            child: const Text('show side sheet'),
-          ),
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: Center(
+        child: FilledButton(
+          onPressed: () {
+            showModalSideSheet(
+              // Required
+              context,
+              // Pass your content widget (required)
+              body: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+
+              // Required
+              header: 'Edit user',
+              barrierDismissible: false,
+              addBackIconButton: false,
+              addCloseIconButton: true,
+              addActions: true,
+              addDivider: true,
+              confirmActionTitle: 'Update',
+              cancelActionTitle: 'Cancel',
+              confirmActionOnPressed: () {
+                Navigator.pop(context);
+                debugPrint('on confirm action');
+              },
+              cancelActionOnPressed: () {
+                Navigator.pop(context);
+                debugPrint('on cancel event');
+              },
+              transitionDuration: const Duration(milliseconds: 500),
+              closeButtonTooltip: 'Close',
+              backButtonTooltip: 'Back',
+              onDismiss: () {
+                debugPrint('on dismiss event');
+              },
+              onClose: () {
+                Navigator.pop(context);
+                debugPrint('on close event');
+              },
+            );
+          },
+          child: const Text('show side sheet'),
         ),
       ),
     );
